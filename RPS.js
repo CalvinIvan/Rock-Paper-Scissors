fcc34e0b89231a1afae7b4f630ee8e864de5e7b5
CalvinIvan <CalvinIvan@users.noreply.github.com>
2020-11-26 20:28:48 -0500

    Add files via upload

--- RPS.js
+++ RPS.js
@@ -1,51 +1,34 @@
-const RPS = [
-    {
+'use strict'
+const RPS = [{
         name: '👊🏽',
         beats: '✌🏽',
-        getname: function() {
-            return this.name;
-        }
     },
     {
         name: '🖐🏽',
         beats: '👊🏽',
-        getname: function() {
-            return this.name;
-        }
+
     },
     {
         name: '✌🏽',
         beats: '🖐🏽',
-        getname: function() {
-            return this.name;
-        }
     }
-]
+];
 
-function winner(userSelect, cSelection) {
-    //alert(userSelect.beats);
-    let a = cSelection;
-    //console.log(userSelect, cSelect());
-    document.getElementById('user-R').innerHTML = userSelect;
-    document.getElementById('comp-R').innerHTML = a;
+
+function compSelection() {
+    let randNum = Math.floor(Math.random() * RPS.length);
+    return RPS[randNum].name;
 }
 
+const userSelection = document.querySelectorAll('[data-value]');
 
-const userSelect = (clicked_id) => {
-    let i = document.getElementById(clicked_id).value;
-    for(let x in RPS) {
-        if(i === RPS[x].name) {
-            i = RPS[x].name;
-            return i;
-        }
-    }
+userSelection.forEach(button => {
+    button.addEventListener('click', (choice) => {
+        const x = button.dataset.value;
+        makeSelection(x);
+    })
+})
+
+function makeSelection(selection) {
+    console.log(selection);
 }
-
-const cSelect = function(){
-    let randNum = Math.floor(Math.random()*RPS.length);
-   let selection = RPS[randNum];
-   return selection.name;
-}
-
-const cSelection = cSelect();
-console.log(cSelection);
